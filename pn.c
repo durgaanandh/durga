05e46524674fa4a99b08ad4ccc18262e82481bae
durgaanandh <37104059+durgaanandh@users.noreply.github.com>
2019-07-02 16:01:31 +0530

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the num");
+scanf("%d" &a);
+if(a>0)
+{
+printf("positive num");
+}
+else if(a<0)
+{
+printf("negative num");
+}
+else()
+{
+printf("zero");
+}
+getch();
+}
