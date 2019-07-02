6b9907dcfc9e957773c1158bf67047f84d3d521c
durgaanandh <37104059+durgaanandh@users.noreply.github.com>
2019-07-02 16:08:31 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the num");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("num is even");
+}
+else
+{
+printf("num is odd");
+}
+getch();
+}
